051d716a7247550b606f469c4f22150831e7b458
Galactic Alien Social Club <freealiensocialclub@gmail.com>
2022-05-01 19:09:27 +0800

    back to normal footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { FiCoffee } from "react-icons/fi";
 import { IoLogoGithub, IoLogoTwitter } from "react-icons/io";
-import { GiSailboat } from "react-icons/gi";
 
 export const Footer = () => {
   return (
     <div className="flex flex-col border-t-1 border w-full p-4 mt-4 items-center text-xs justify-center">
-    
+
       <a
         className="flex items-center mb-4 text-[#202520] hover:text-[#575b57]"
         href="https://twitter.com/GalacticAlienSC"
@@ -18,16 +17,6 @@ export const Footer = () => {
         Twitter
       </a>
 
-      <a
-        className="flex items-center mb-4 text-[#202520] hover:text-[#575b57]"
-        href="https://opensea.io/collection/galacticaliensocialclub"
-        target="_blank"
-      >
-        <span className="text-lg mr-1">
-          <GiSailboat />
-        </span>
-        OpenSea
-      </a>
 
     </div>
   );
