dd80dcf772fd95cb90939974f892f8bda990e5a6
Galactic Alien Social Club <freealiensocialclub@gmail.com>
2022-05-22 23:48:11 +0800

    no more logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ import { IoMdClose } from 'react-icons/io';
 function Navbar({ title, menu, showMenu, setShowMenu }) {
   return (
     <div className="bg-[#95aedf] w-full flex justify-between items-center p-4">
-      <img src="/logo.png">
+      <h1 className="text-lg text-[#202520] font-semibold">{title}  </h1>
       {menu &&
         <div className="md:hidden text-md text-black" onClick={() => setShowMenu(!showMenu)}>
           {showMenu ? <IoMdClose /> : <GiHamburgerMenu />}
