ad12526254df4c611c1f105625252ee221f1de7d
Galactic Alien Social Club <freealiensocialclub@gmail.com>
2022-05-22 23:44:16 +0800

    img logo and description changes

--- pages/api/project.js
+++ pages/api/project.js
@@ -2,6 +2,6 @@
 
 export default function projectAPI(req, res) {
     const { name } = req.query
-    let description ="GASC is an NFT collection of 10,000 provably unique, hand-drawn, pixel art Aliens programmatically generated with over 300+ attributes and traits stored on the Ethereum blockchain.";
+    let description ="GASC is a donation-oriented NFT collection of 10,000 provably unique, hand-drawn, pixel art Aliens programmatically generated with over 300+ attributes and traits stored on the Ethereum blockchain.";
     res.status(200).json({ name, description })
 }
