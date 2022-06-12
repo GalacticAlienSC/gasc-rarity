62b4fdca57a6daee142f34ea54264a0fc9692287
Galactic Alien Social Club <freealiensocialclub@gmail.com>
2022-06-12 16:35:56 +0800

    opensea button test

--- pages/[id].js
+++ pages/[id].js
@@ -6,6 +6,7 @@ import { useRouter } from "next/router";
 import Navbar from "../components/Navbar";
 import { config } from "../config";
 import { Footer } from "../components/Footer";
+import { GiSailboat } from "react-icons/gi";
 
 const Trait = (attribute) => {
   return (
@@ -80,6 +81,16 @@ function NFT({ nft, title }) {
               Ranking Score: {nft.rarity_score.toFixed(2)} ✪
             </div>
             <div className="py-4 px-2 w-full rounded-md text-lg mt-4 bg-[#a0c2ea] text-[#202520]">
+              <a
+              className="flex items-center mb-4 text-[#202520] hover:text-[#575b57]"
+              href={nft?.opensea_url}
+              target="_blank"
+              >
+              <span className="text-lg mr-1">
+              <GiSailboat />
+              </span>
+              OpenSea
+              </a>
               Ranking Score: {nft.rarity_score.toFixed(2)} ✪
             </div>
             {nft.current_price !== "-" && (
